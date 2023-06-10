7ffba4f3972ee921e48cdb1fb76cff0a013a0472
N0zzy <ffnn83@yandex.ru>
2023-06-11 00:57:12 +0500

    add test connect, pressed button and glpchp

--- GodotLibrary/N0zzy/src/Application/UxWindow.php
+++ GodotLibrary/N0zzy/src/Application/UxWindow.php
@@ -3,32 +3,60 @@ namespace N0zzy\Application;
 
 use Godot\GD;
 use Godot\NodePath;
+use Godot\PopupMenu;
 use Godot\SceneTree;
 use Godot\Engine;
 use Godot\Node;
 use Godot\Button;
 use Godot\Node2D;
+use Godot\SignalName;
+use Pchp\Core\CallableCallback;
 use System\Exception;
 use System\Boolean;
-
+use Godot\Signal;
+use Godot\StringName;
+use GLPchp\CallableFunc;
 
 class UxWindow extends UxWindowAbstract {
+    protected $sceneTree;
+    protected $button;
+    protected $call = null;
 
-    public function _Ready() : void {	
-        /**$this->injectSdk(); //only built-in**/
-        $a = $this->GetTree();
-        GD::Print(777);
+    public function _Ready() : void {
+        $this->sceneTree = $this->GetTree();
+        $this->call = new CallableFunc();
+        $this->button = $this->getButton("./Control/Button");
+        $this->button->Text = "New Button";
+        $this->exampleEvents_buttonPressed_3();
     }    
 
     public function _Process($delta) : void  {
 
     }
 
-    protected function getButton(){
-
+    /**
+     * @param string $path
+     * @return Button|Node
+     */
+    protected function getButton(string $path): Button|Node {
+        $nodePath = new NodePath($path);
+        return $this->sceneTree->CurrentScene->GetNode<Button>($nodePath);
     }
     
-    /** only built-in
-    protected function injectSdk(){try { new \Php\Sdk\Sdk(); } catch (Exception $e) {}}
-    */
+    protected function exampleEvents_buttonPressed_1() {
+        $this->call->On("pressed", $this->button, function(){
+            $this->button->Text = __METHOD__;
+        }); 
+    }
+    
+    protected function exampleEvents_buttonPressed_2() {
+        $this->call->OnListener("pressed", $this->button, [$this, "OnButtonPressed"]); 
+    }
+    
+    protected function exampleEvents_buttonPressed_3() {
+        $a = __METHOD__;            
+        $this->button->Connect(new StringName("pressed"), CallableFunc::Get(function() use ($a){
+            $this->button->Text = $a;
+        }));
+    }
 }
--- GodotLibrary/N0zzy/src/Application/UxWindowAbstract.php
+++ GodotLibrary/N0zzy/src/Application/UxWindowAbstract.php
@@ -2,8 +2,15 @@
 namespace N0zzy\Application;
 
 use Godot\Node2D;
+use Godot\GD;
+
 
 abstract class UxWindowAbstract extends Node2D {
     public function _Ready() : void {}        
     public function _Process($delta) : void {}
+    
+    public function OnButtonPressed(): void
+    {
+        $this->button->Text = __METHOD__;       
+    }
 }
