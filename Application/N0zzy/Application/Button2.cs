d6ea144d6090475c2ea4dbca691c52e51dc6a5cb
n0zzy <46209742+N0zzy@users.noreply.github.com>
2023-06-22 15:32:01 +0500

    Delete Application directory

--- Application/N0zzy/Application/Button2.cs
+++
@@ -1,7 +0,0 @@
-namespace N0zzy.Application;
-
-public partial class Button2 : UxButton2 {
-
-	public override void _Ready() {  base._Ready(); }
-
-}
--- Application/N0zzy/Application/Window.cs
+++
@@ -1,7 +0,0 @@
-namespace N0zzy.Application;
-
-public partial class Window : UxWindow {
-
-	public override void _Ready() {  base._Ready(); }
-
-}
