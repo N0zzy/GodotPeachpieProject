c5ddc2dd7fd771bec52b9ab5fcda52a1cd5e590c
N0zzy <ffnn83@yandex.ru>
2024-07-30 16:47:36 +0500

    generate ux

---
+++ project/application/gui/ButtonIdTest.cs
@@ -0,0 +1,12 @@
+using GodotPeachpie.Gui;
+using Godot;
+public partial class ButtonIdTest : UxButtonIdTest
+{
+		public override void _on_pressed(object a)
+		{
+			base._on_pressed(a);
+		}
+		public override void _on_set(object x, object y)
+		{
+			base._on_set(x, y);
+		}
