f7cd6e707466be5be11773b51431edb65d498d24
N0zzy <ffnn83@yandex.ru>
2024-08-03 22:52:10 +0500

    GPL-library, examples TextEdit, support class UiNode

--- project/library/src/Gui/UxButtonIdTest.php
+++ project/library/src/Gui/UxButtonIdTest.php
@@ -2,13 +2,34 @@
 namespace GodotPeachpie\Gui;
 
 use Godot\Button; 
+use Godot\TextEdit; 
 use Godot\GD; 
+use GPL\UiNode;
 
-class UxButtonIdTest extends Button {
+class UxButtonIdTest extends Button 
+{
+    private ?TextEdit $textEdit1 = null;
+    private ?TextEdit $textEdit2 = null;
+    
+    public function makeTextEdit() 
+    {
+        //example 1
+        $this->textEdit1 ??= UiNode::Get($this, "/root/Main/TextEdit");
+        //example 2
+        $this->textEdit2 ??= UiNode::Get($this, "../TextEdit");     
+    }
+    
     /**
      * @signal
      */
-    public function _on_pressed() : void {
+    public function _on_pressed() : void 
+    {   
+        $this->makeTextEdit(); 
+   
         GD::Print("UxButtonIdTest pressed!!!");
+        GD::Print($this->getPath());  
+        
+        $this->textEdit1->Text = "UxButtonIdTest 1 pressed!!!";
+        $this->textEdit2->Text = "UxButtonIdTest 2 pressed!!!";
     }
 }
